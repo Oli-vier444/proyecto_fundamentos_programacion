c7a8f329bba59ce62730c8dac971f6e4aa517f95
Oli-vier <167052360+Oli-vier444@users.noreply.github.com>
2026-08-30 20:42:27 -0600

    arreglar indentación de las últimas 4 líneas

--- algoritmo_inicial_proyecto_fundamentos.py
+++ algoritmo_inicial_proyecto_fundamentos.py
@@ -33,9 +33,9 @@ Algoritmo “Organización y automatización de correos electrónicos”
 9.	SINO
     a.	PREGUNTAR si desea salir
     b.	GUARDAR en desea_salir
-c.	SI desea_salir == 'si'
-    i.	FIN
-d.	SINO
-    i.	REGRESAR a paso 4
+	c.	SI desea_salir == 'si'
+    	i.	FINSI
+	d.	SINO
+    	i.	REGRESAR a paso 4
 10.	FIN
 """
